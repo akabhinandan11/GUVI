781947387844e214fe4ac639d1e9ccc5e64bf442
Abhinandan Kumar <48753305+akabhinandan11@users.noreply.github.com>
2019-05-26 00:54:02 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
-n=int(input())
+q=int(input())
 m=1000
 for i in range(0,20):
-    if pow(2,i)<=n:
-        x = abs(pow(2, i) - n)
+    if pow(2,i)<=q:
+        x = abs(pow(2, i) - q)
         if x < m: m = x
 print(m)
