d008bfa57325abbb8bb2c444f87fa4b967ebdee3
Abhinandan Kumar <48753305+akabhinandan11@users.noreply.github.com>
2019-05-26 00:39:33 +0530

    Create commomprefix.py

---
+++ commomprefix.py
@@ -0,0 +1,12 @@
+q=int(input())
+l=[]
+for i in range(0,q):
+    i=input()
+    l.append(i)
+l1=[]
+for i in zip(*l):
+    if i.count(i[0])==len(i):
+        l1.append(i[0])
+    else:
+        break
+print(''.join(l1))
