6feaef348a2356f737adaee8a94810dc55a0767c
Abhinandan Kumar <48753305+akabhinandan11@users.noreply.github.com>
2019-06-03 22:32:42 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,11 @@
+z=int(input())
+k=list(map(int,input().split()))
+s=[1]*z
+for i in range(z):
+    if i==0:
+        if k[i]>k[i+1]:
+            s[i]=s[i]+s[i+1]
+    elif i>0:
+        if k[i]>k[i-1]:
+            s[i]=s[i]+s[i-1]
+print(sum(s))
